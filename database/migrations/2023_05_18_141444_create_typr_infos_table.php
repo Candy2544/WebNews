984be8d5f6f568feeaf74698a301e07c794b0bb5
Candy2544 <s6406021421031@email.kmutnb.ac.th>
2023-06-07 18:04:03 +0700

    upload img

--- database/migrations/2023_05_18_141444_create_typr_infos_table.php
+++ database/migrations/2023_05_18_141444_create_typr_infos_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('typr_infos', function (Blueprint $table) {
             $table->integer('id_type');
             $table->varchar('type');
-        
         });
     }
 
